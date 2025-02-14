f870dbea2d1db934a1a01f48357fc86518468917
Kpodedji <162030419+Kpodedji@users.noreply.github.com>
2025-02-14 17:33:29 +0100

    audit.sh

---
+++ Initial_setup/Mandatory_access_control/Configure_appamor/AppArmor_Profiles/audit.sh
@@ -0,0 +1,16 @@
+AppArmor Profiles are in enforce 
+37 profiles are loaded.
+35 profiles are in enforce mode.
+2 profiles are in complain mode.
+4 processes have profiles defined.
+37 profiles are loaded.
+35 profiles are in enforce mode.
+2 profiles are in complain mode.
+4 processes have profiles defined.
+37 profiles are loaded.
+35 profiles are in enforce mode.
+2 profiles are in complain mode.
+4 processes have profiles defined.
+
+
+
