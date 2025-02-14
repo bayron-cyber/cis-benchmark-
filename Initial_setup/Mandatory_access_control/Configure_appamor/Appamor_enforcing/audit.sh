77e454833b3168ee0c4dacedb7f3f36cffda5641
Kpodedji <162030419+Kpodedji@users.noreply.github.com>
2025-02-14 17:38:15 +0100

    audit.sh

---
+++ Initial_setup/Mandatory_access_control/Configure_appamor/Appamor_enforcing/audit.sh
@@ -0,0 +1,13 @@
+# apparmor_status | grep profiles
+34 profiles are loaded.
+34 profiles are in enforce mode.
+0 profiles are in complain mode.
+2 processes have profiles defined.
+34 profiles are loaded.
+34 profiles are in enforce mode.
+0 profiles are in complain mode.
+2 processes have profiles defined.
+2 processes have profiles defined.
+2 processes are in enforce mode.
+0 processes are in complain mode.
+0 processes are unconfined but have a profile defined.
