17f6ca09846fa1d8385701d160778e3c59a66f1d
bayron-cyber <emmanueladounvo408@gmail.com>
2025-02-14 18:54:48 +0100

    remediation.sh

---
+++ Initial_setup/Configure_ Additional_Process_Hardening/error_reporting/remediation.sh
@@ -0,0 +1,7 @@
+Run the following commands to stop and mask the apport service
+# systemctl stop apport.service 
+# systemctl mask apport.service
+- OR -
+Run the following command to remove the apport package:
+
+# apt purge apport
