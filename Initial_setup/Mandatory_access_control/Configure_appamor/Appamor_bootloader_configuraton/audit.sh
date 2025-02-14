97de26c5398e74c4ca46d4791ea80f1621632a58
Kpodedji <162030419+Kpodedji@users.noreply.github.com>
2025-02-14 17:26:19 +0100

    audit.sh

---
+++ Initial_setup/Mandatory_access_control/Configure_appamor/Appamor_bootloader_configuraton/audit.sh
@@ -0,0 +1,4 @@
+# grep "^\s*linux" /boot/grub/grub.cfg | grep -v "apparmor=1"
+Nothing should be returned
+# grep "^\s*linux" /boot/grub/grub.cfg | grep -v "security=apparmor"
+Nothing should be returned
