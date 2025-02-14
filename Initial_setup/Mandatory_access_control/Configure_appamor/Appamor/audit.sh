e4930fa57d09b9a2514af3ce0c74935d49057eaf
Kpodedji <162030419+Kpodedji@users.noreply.github.com>
2025-02-14 17:21:55 +0100

    audit.sh

---
+++ Initial_setup/Mandatory_access_control/Configure_appamor/Appamor/audit.sh
@@ -0,0 +1,4 @@
+# dpkg-query -s apparmor &>/dev/null && echo "apparmor is installed"
+apparmor is installed
+# dpkg-query -s apparmor-utils &>/dev/null && echo "apparmor-utils is installed"
+apparmor-utils is installed
