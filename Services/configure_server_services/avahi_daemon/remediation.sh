d861e1495457139c97bc6a7600ef44ee87a8b666
bayron-cyber <emmanueladounvo408@gmail.com>
2025-02-16 11:41:52 +0100

    remediation.sh

---
+++ Services/configure_server_services/avahi_daemon/remediation.sh
@@ -0,0 +1,5 @@
+# systemctl stop avahi-daemon.socket avahi-daemon.service
+# apt purge avahi-daemon
+or
+# systemctl stop avahi-daemon.socket avahi-daemon.service
+# systemctl mask avahi-daemon.socket avahi-daemon.service
